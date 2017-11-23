4c7b94cbb809fe722e627e0946df06f4bae0c6a7
Rozhoň Ivan <irozhon@trask.cz>
2017-11-23 16:14:06 +0100

    ES5 refactoring

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const cmd = require('node-cmd');
-const flags = require('flags');
+var path = require('path');
+var cmd = require('node-cmd');
+var flags = require('flags');
 
 // get the path of PHP source folder
-const phpDir = path.resolve(__dirname, 'php-7.1.11-Win32-VC14-x64');
+var phpDir = path.resolve(__dirname, 'php-7.1.11-Win32-VC14-x64');
 
 // define posible flags (port, dir)
 flags.defineString('port');
@@ -12,21 +12,19 @@ flags.defineString('dir');
 // get (parse) flags
 flags.parse();
 // default port is :8000
-const port = flags.get('port') ? flags.get('port') : '8000';
-var dir = flags.get('dir'),
-    dirCommand = dir ? ` -t ${dir}` : '';
+var port = flags.get('port') ? flags.get('port') : '8000';
+var dir = flags.get('dir');
+var dirCommand = dir ? ' -t ' + dir : '';
 
 // run command to start PHP built-in server with specific port
 cmd.get(
-    `${phpDir}/php.exe -S localhost:${port}${dirCommand}`,
+    phpDir + '/php.exe -S localhost:' + port + dirCommand,
     function (err, data, stderr) {
         console.log(arguments);
     }
 );
 
 // log information about successful start
-console.log(`
-    PHP 7.1.11 Development Server started.
-    Listening on http://localhost:${port}
-    Press Ctrl-C to quit.
-`);
+console.log('PHP 7.1.11 Development Server started.');
+console.log('Listening on http://localhost:' + port);
+console.log('Press Ctrl-C to quit.');
