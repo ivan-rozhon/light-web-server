e1afe8b4683a3b68b0f0bc36a34bda34b5bfabe0
Rozhoň Ivan <irozhon@trask.cz>
2017-11-24 12:02:16 +0100

    improve travis.yml

--- test/test.js
+++ test/test.js
@@ -18,3 +18,8 @@ test('port and dist flags defined', function (t) {
   t.is(testCommand, 'php-dir/php.exe -S localhost:9000 -t dist/')
   t.end()
 })
+
+// test('just run "run()" method/function', function (t) {
+//   run()
+//   t.end()
+// })
