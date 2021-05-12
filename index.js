ab173a853f368c58b2289da472d4b540fe655fe9
Moricz, Ferenc <fmoricz@gmail.com>
2021-05-12 18:55:17 +0200

    Added parameter: hostname

--- index.js
+++ index.js
@@ -11,26 +11,28 @@ function run () {
   // check whether platform is macOS
   var isMacOS = process.platform === 'darwin'
 
-  // define possible flags (port, dir)
+  // define possible flags (hostname, port, dir)
+  flags.defineString('hostname')
   flags.defineString('port')
   flags.defineString('dir')
 
   // get (parse) flags
   flags.parse()
   // default port is :8000
+  var hostname = flags.get('hostname') || 'localhost'
   var port = flags.get('port') || '8000'
   var dir = flags.get('dir')
 
   var job
 
   if (isMacOS) {
-    job = execCommand(getCommand('', port, dir, true))
+    job = execCommand(getCommand('', hostname, port, dir, true))
   } else {
     // get the path of PHP source folder
     var phpDir = path.resolve(__dirname, 'php-7.1.11-Win32-VC14-x64')
 
     // create command
-    var command = getCommand(phpDir, port, dir)
+    var command = getCommand(phpDir, hostname, port, dir)
 
     // run command to start PHP built-in server with specific port
     job = execCommand(command)
@@ -39,7 +41,7 @@ function run () {
   if (job) {
     // log information about successful start
     console.log(isMacOS ? 'PHP Development Server started.' : 'PHP 7.1.11 Development Server started.')
-    console.log('Listening on http://localhost:' + port)
+    console.log('Listening on http://' + hostname + ':' + port)
     console.log('Press Ctrl-C to quit.')
   }
 
@@ -53,12 +55,12 @@ function execCommand (command) {
 }
 
 // compound terminal command for starting web server
-function getCommand (phpDir, port, dir, isMacOS) {
+function getCommand (phpDir, hostname, port, dir, isMacOS) {
   // prepare flag for document root directory
   var dirCommand = dir ? ' -t ' + dir : ''
 
   // compound all together
-  return isMacOS ? 'php -S localhost:' + port + dirCommand : phpDir + '/php.exe -S localhost:' + port + dirCommand
+  return isMacOS ? 'php -S ' + hostname + ':' + port + dirCommand : phpDir + '/php.exe -S ' + hostname + ':' + port + dirCommand
 }
 
 // export run method as node module
